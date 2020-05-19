bb9d9471031268a369f81074ddfe210d0a621478
Konstantin Sharykin <kotyshe@gmail.com>
2020-05-20 01:01:03 +0300

    Changed building

---
+++ ItProjectStart/ItProjectStart/src/main/java/com/example/ItProjectStart/ItProjectStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.ItProjectStart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItProjectStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItProjectStartApplication.class, args);
+	}
+
+}
---
+++ ItProjectStart/ItProjectStart/src/test/java/com/example/ItProjectStart/ItProjectStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ItProjectStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItProjectStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
