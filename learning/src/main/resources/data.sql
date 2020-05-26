98389d80e9f2722a102c11d685ddb1850c469913
Mark <christmas2491@gmail.com>
2020-05-26 18:53:04 +0300

    added parser for body of output html

--- learning/src/main/resources/data.sql
+++ learning/src/main/resources/data.sql
@@ -5,6 +5,10 @@ VALUES ('5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8', 'krh
        ('4813494D137E1631BBA301D5ACAB6E7BB7AA74CE1185D456565EF51D737677B2', 'rooted', 1),
        ('8C6976E5B5410415BDE908BD4DEE15DFB167A9C873FC4BB8A81F6F2AB448A918', 'admin', 1);
 
+INSERT INTO user_role(user_id, role) VALUES (3, 'ADMIN'),
+                                            (2, 'USER'),
+                                            (3, 'USER');
+
 INSERT INTO list_task(id, level_task, name, previous_id)
 VALUES (1, 1, 'Day1', NULL), (2, 2, 'Day2', 1), (3, 3, 'Day3', 2);
 
